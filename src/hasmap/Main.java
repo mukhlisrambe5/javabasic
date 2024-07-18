bb573b5eeff4bad34a262947c886732ce7a42569
mukhlisrambe5 <mukhlis.rambe5@gmail.com>
2024-07-18 07:08:08 +0700

    done hashmap

--- src/hasmap/Main.java
+++ src/hasmap/Main.java
@@ -4,6 +4,16 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap <> countries = new HashMap<>();
+        HashMap <String, String> countries = new HashMap<String, String>();
+
+        //add a key and value
+        countries.put("USA", "Washington D.C.");
+        countries.put("Canada", "Ottawa");
+        countries.put("France", "Paris");
+
+        for (String country : countries.keySet()){
+            System.out.println("Key: " + country + ", Value: " + countries.get(country));
+        }
+
     }
 }
